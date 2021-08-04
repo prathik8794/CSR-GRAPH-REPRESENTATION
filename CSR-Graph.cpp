3109322d5f25af7977bab471717e68342198f66f
Prathik P Kulkarni <83969842+prathik8794@users.noreply.github.com>
2021-08-04 17:56:09 +0530

    Update CSR-Graph.cpp

--- CSR-Graph.cpp
+++ CSR-Graph.cpp
@@ -35,7 +35,7 @@ class CSR{
                 cout<<edge[j]<<" ";
             }*/
     }
-    vector<int> bfs(int s,int e){
+    vector<int> bfspath(int s,int e){
         queue<int>q;
         vector<bool>visited(nodes,false);
         vector<int>prev(nodes,-1);
@@ -60,21 +60,52 @@ class CSR{
           return path;
          
     }
+    void bfs_algo(vector<bool>&v,int i){
+        queue<int>q;
+        q.push(i);
+        v[i]=true;
+        while (!q.empty())
+        {
+            int x = q.front();
+            q.pop();
+            for (int i = vertex[x]; i < vertex[x + 1]; i++)
+            {
+                if (!v[edge[i]])
+                {
+                    v[edge[i]] = true;
+                    q.push(edge[i]);
+                }
+            }
+        }
+    }
+    int graphcomponent(){
+            vector<bool>check(nodes,0);
+            int components=0;
+            for(int i=0;i<nodes;i++){
+                if(check[i]==false){
+                      bfs_algo(check,i);  
+                      components++;
+                }
+            }
+            return components;
+    }
 
 };
 int main(){
     CSR g1;
-    int nc,s,e;
-    cin>>nc>>s>>e;
+    int nc;
+    cin>>nc;
     g1.nodes=nc;
     g1.constructCSR();
     g1.display();
     cout<<endl;
 
-    vector<int>result=g1.bfs(s,e);
+    /*vector<int>result=g1.bfspath(s,e);
     for(auto i:result){
         cout<<i<<" ";
-    }
+    }*/
+    int grapgcomponent = g1.graphcomponent();
+    cout<<grapgcomponent<<endl;
 }
 
 /*
